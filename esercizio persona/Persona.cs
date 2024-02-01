ebcb938e634a612dd4bd0cd5c49b1ed423e99e77
nipol <nipol@LAPTOP-5ALMU75U>
2024-02-01 20:13:06 +0100

    Aggiungere i file di progetto.

---
+++ esercizio persona/Persona.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esercizio_persona
+{
+    internal class Persona
+    {
+        public string Nome { get; set; }
+        public int Eta { get; set; }
+
+        public Persona(string nome, int eta)
+        {
+            Nome = nome;
+            Eta = eta;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome},{Eta}";
+        }
+    }
+}
---
+++ esercizio persona/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esercizio_persona
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Persona> persone = new List<Persona>();
+            int n;
+            Console.WriteLine("quante persone? ");
+            n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine("inserisci nome ed eta della persona {0}", i + 1);
+                string nome = Console.ReadLine();
+                int eta = int.Parse(Console.ReadLine());
+                persone.Add(new Persona(nome, eta)); //aggiungi persone alla lista
+            }
+
+            foreach(Persona persona in persone)
+            {
+                if (persona.Eta > 17)
+                    Console.WriteLine(persona);
+                Console.ReadLine();
+            }
+        }
+    }
+}
